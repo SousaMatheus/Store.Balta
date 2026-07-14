206ecf534f7724d1fa56a6d9fd772f561c1c29b1
Matheus Sousa <ma.sousa.soares@gmail.com>
2026-07-13 23:30:23 -0300

    feat: add EOrdemStatus and Order entities

---
+++ src/Store.Balta.Domain/Store.Domain/Entities/Order.cs
@@ -0,0 +1,53 @@
+﻿using Store.Balta.Domain.Store.Domain.Enums;
+
+namespace Store.Domain.Entities;
+
+public class Order : Entity
+{
+    public Order(Customer customer, Discount discount, decimal deliveryFee)
+    {
+        Customer = customer;
+        Discount = discount;
+        DeliveryFee = deliveryFee;
+        Date = DateTime.Now;
+        Number = Guid.NewGuid().ToString().Substring(0, 8);
+        Status = EOrdemStatus.WaitingPayment;
+        Items = new List<OrderItem>();
+    }
+
+    public Customer Customer { get; private set; }
+    public DateTime Date { get; private set; }
+    public string Number { get; private set; }
+    public IList<OrderItem> Items { get; private set; }
+    public EOrdemStatus Status { get; private set; }
+    public decimal TotalPrice { get; private set; }
+    public Discount Discount { get; private set; }
+    public decimal DeliveryFee { get; private set; }
+
+    public void AddItens(Product product, int quantity)
+    {
+        var item = new OrderItem(product, quantity);
+        Items.Add(item);
+    }
+
+    public decimal Total()
+    {
+        decimal total = Items.Sum(item => item.Total());
+
+        total += DeliveryFee;
+        total -= Discount != null ? Discount.Value() : 0;
+
+        return total;
+    }
+
+    public void Pay(decimal amount)
+    {
+        if (amount == Total())
+            Status = EOrdemStatus.WaitingDelivery;
+    }
+
+    public void Cancel()
+    {
+        Status = EOrdemStatus.Cancelled;
+    }
+}
---
+++ src/Store.Balta.Domain/Store.Domain/Enums/EOrdemStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Store.Balta.Domain.Store.Domain.Enums;
+
+public enum EOrdemStatus
+{
+    WaitingPayment = 1,
+    WaitingDelivery = 2,
+    Cancelled = 3,
+    Completed = 4,
+}
